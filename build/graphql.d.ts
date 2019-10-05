cb189fa923960d8cfba11ff0712dc7bd0ed8a340
Aizat Faiz <aizat.faiz@gmail.com>
2019-10-05 11:01:14 +0800

    init

---
+++ build/graphql.d.ts
@@ -0,0 +1,10 @@
+export declare const connectionArgs: any;
+export declare function connectionDefinitions(config: any): any;
+export declare const argsToSortAndOrder: (args: any) => {
+    column: any;
+    direction: any;
+};
+export declare function fieldsFromInfo(info: any): Set<string>;
+export declare function addArgsToQuery(args: any, query: any): any;
+export declare function connectionFromKnex<T>(args: any, query: any, countQuery: any, info: any): Promise<any>;
+//# sourceMappingURL=graphql.d.ts.map
---
+++ src/graphql.ts
@@ -0,0 +1,157 @@
+import { GraphQLInt, GraphQLNonNull, GraphQLString } from "graphql";
+
+import {
+  connectionArgs as connectionArgsBase,
+  connectionDefinitions as connectionDefinitionsBase,
+  connectionFromArraySlice,
+  cursorToOffset
+} from "graphql-relay";
+
+export const connectionArgs = {
+  order: {
+    type: GraphQLString
+  },
+  sort: {
+    type: GraphQLString
+  },
+  ...connectionArgsBase
+};
+
+export function connectionDefinitions(config) {
+  // eslint-disable-next-line
+  config.connectionFields = {
+    totalCount: {
+      type: new GraphQLNonNull(GraphQLInt)
+    }
+  };
+  return connectionDefinitionsBase(config);
+}
+
+function connectionArgsToLimitAndOffset(args) {
+  // HANDLE LAST
+  const { after, before, first, last } = args;
+
+  if (last && before) {
+    const offset = cursorToOffset(before) - last;
+    return {
+      limit: last,
+      offset: offset > 0 ? offset : 0
+    };
+  }
+
+  if (after) {
+    const offset = cursorToOffset(after);
+    return {
+      limit: first,
+      offset
+    };
+  }
+
+  return {
+    limit: first,
+    offset: 0
+  };
+}
+
+export const argsToSortAndOrder = args => {
+  const direction = args.order ? args.order : "asc";
+  const column = args.sort ? args.sort : "uuid";
+
+  return { column, direction };
+};
+
+function fieldsFromSelectionSet(info, selectionSet) {
+  let fields = new Set<string>();
+
+  selectionSet.selections.forEach(field => {
+    const name = field.name.value;
+    switch (field.kind) {
+      case "FragmentSpread": {
+        const fields2 = fieldsFromSelectionSet(
+          info,
+          info.fragments[name].selectionSet
+        );
+        fields = new Set([...fields, ...fields2]);
+        break;
+      }
+
+      case "Field":
+        fields.add(name);
+        break;
+
+      default:
+        break;
+    }
+  });
+
+  return fields;
+}
+
+export function fieldsFromInfo(info): Set<string> {
+  if (
+    !info ||
+    !info.fieldNodes ||
+    !Array.isArray(info.fieldNodes) ||
+    info.fieldNodes.length === 0
+  ) {
+    return new Set();
+  }
+
+  let fields = new Set<string>();
+
+  // eslint-disable-next-line no-restricted-syntax
+  for (const fieldNode of info.fieldNodes) {
+    const newFields = fieldsFromSelectionSet(info, fieldNode.selectionSet);
+
+    fields = new Set<string>([...fields, ...newFields]);
+  }
+
+  return fields;
+}
+
+export function addArgsToQuery(args, query) {
+  const { limit, offset } = connectionArgsToLimitAndOffset(args);
+
+  if (limit) {
+    query.limit(limit);
+  }
+
+  return query;
+}
+
+export async function connectionFromKnex<T>(
+  args,
+  query: any,
+  countQuery: any,
+  info
+): Promise<any> {
+  const { offset } = connectionArgsToLimitAndOffset(args);
+  const whereQuery = addArgsToQuery(args, query);
+
+  if (offset) {
+    query.offset(offset);
+  }
+
+  if (info) {
+    const fields = fieldsFromInfo(info);
+    if (fields.size === 1 && fields.has("totalCount")) {
+      const [{ count }] = await countQuery;
+
+      return {
+        totalCount: count
+      };
+    }
+  }
+
+  const [rows, [{ count }]] = await Promise.all([whereQuery, countQuery]);
+
+  const values = connectionFromArraySlice<T>(rows, args, {
+    sliceStart: offset,
+    arrayLength: count
+  });
+
+  return {
+    ...values,
+    totalCount: count
+  };
+}
